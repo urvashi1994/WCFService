f683de9ddb20c36d8a444b3502e58f9a4c35440b
urvashipatel9734 <urvashipatel9734@gmail.com>
2021-12-12 16:47:21 -0500

    Add project files.

---
+++ WCFHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            using (ServiceHost host = new ServiceHost(typeof(WCFService.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Host has been Started!!!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
